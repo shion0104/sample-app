e1d6c7b62ec5283ca916d8b0f5cf09376b03b5f5
shion0104 <zhiyinzhongze@gmail.com>
2024-11-18 15:15:28 +0900

    9.1.3ユーザーを忘れる

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,30 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-    def log_in(user)
-      session[:user_id] = user.id
-    end
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
-    def remember(user)
-        user.remember
-        cookies.permanent.encrypted[:user_id] = user.id
-        cookies.permanent[:remember_token] = user.remember_token
-      end
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
-    # 現在ログイン中のユーザーを返す（いる場合）
+  # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id: user_id)
-      elsif (user_id = cookies.encrypted[:user_id])
-        user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
-          log_in user
-          @current_user = user
-        end
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -29,7 +34,8 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     reset_session
-    @current_user = nil   # 安全のため
+    @current_user = nil # 安全のため
   end
 end
