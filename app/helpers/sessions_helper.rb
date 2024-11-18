6529e40c79aef6ec676129c6bc8121fb02a268e7
shion0104 <zhiyinzhongze@gmail.com>
2024-11-18 17:24:11 +0900

    9章完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,7 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
   def forget(user)
@@ -18,7 +19,11 @@ module SessionsHelper
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+        user = User.find_by(id: user_id)
+        debugger
+        if user && session[:session_token] == user.session_token
+          @current_user = user
+        end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
