b560660e29a8c79e512cd45e9766563fd2e077a9
shion0104 <zhiyinzhongze@gmail.com>
2024-11-18 15:41:06 +0900

    9.1.42つの目立たないバグの修正

--- db/migrate/20241112085838_add_password_digest_to_users.rb
+++ db/migrate/20241112085838_add_password_digest_to_users.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# This migration adds the password_digest column to the users table
+# to store securely hashed passwords for user authentication.
+
 class AddPasswordDigestToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :password_digest, :string
