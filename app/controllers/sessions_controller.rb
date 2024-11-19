541e2ac3036bf9e3ae2f7af0d8826e81c7e410d4
shion0104 <zhiyinzhongze@gmail.com>
2024-11-19 18:23:25 +0900

    ユーザーの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      debugger
       forwarding_url = session[:forwarding_url]
       reset_session
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
