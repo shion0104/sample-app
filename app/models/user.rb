0283d5948ab4a99c3db3f94d0eba7252db2e4c6e
shion0104 <zhiyinzhongze@gmail.com>
2024-11-26 15:10:44 +0900

    14.1.5フォロワー

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed                               
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower                           
 
   def self.digest(string)
     cost = if ActiveModel::SecurePassword.min_cost
